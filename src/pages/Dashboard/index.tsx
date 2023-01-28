2bf06d2d374a52ab1d2e30a1611007a2e14d7e06
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-01-28 15:40:45 -0300

    Create Dashboard functions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import { Card } from "../../components/Card"
+import { Agendamento } from "../Agendamentos/types";
 import * as C from './styles'
 
 export const Dashboard = () => {
+    const [agendamentos, setAgendamentos] = useState<Agendamento[]>([]);
+
+    useEffect(() => {
+        const storedAgendamentos = JSON.parse(localStorage.getItem('agendamentos') || '[]');
+        setAgendamentos(storedAgendamentos);
+    }, []);
+
+    
+    const isChecked = agendamentos.map((_, index) =>
+        JSON.parse(localStorage.getItem(`checkbox_${index}`) || 'false')
+    );
+
+    const sumAll = agendamentos.reduce((acc, item) => acc + Number(item.valor), 0)
+
     return (
         <div>
-        <C.CardContainer>
-            <C.CardWrapper><Card title="Agendamentos do dia" text="Texto do card" /></C.CardWrapper>
-            <C.CardWrapper><Card title="Pacientes atendidos no dia" text="Texto do card" /></C.CardWrapper>
-            <C.CardWrapper><Card title="Faturamento do dia" text="Texto do card" /></C.CardWrapper>
-        </C.CardContainer>
+            <C.CardContainer>
+                <C.CardWrapper><Card title="Agendamentos do dia" text={agendamentos.length.toString()} /></C.CardWrapper>
+                <C.CardWrapper><Card title="Pacientes atendidos no dia" text={isChecked.filter(Boolean).length.toString()} /></C.CardWrapper>
+                <C.CardWrapper><Card title="Faturamento do dia" text={sumAll.toString()} /></C.CardWrapper>
+            </C.CardContainer>
         </div>
     )
 }
